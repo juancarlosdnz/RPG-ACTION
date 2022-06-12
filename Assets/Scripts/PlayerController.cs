cf9646047a421e9f5a662ec6be3361896b1276ff
juancarlosdnz <dnzjuancarlos@gmail.com>
2022-06-12 23:02:21 +0100

    Run animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     private PlayerInput playerInput;
     private Transform playerTransform;
+    private Rigidbody2D playerRB;
+    private Animator animator;
+    private SpriteRenderer playerSprite;
+
     private float horizontal;
     private float vertical;
-    private Rigidbody2D playerRB;
-    private bool backSide;
-    private Animator animator;
+    int walkHashCode;
 
     [SerializeField] private float velocidad;
 
@@ -18,35 +20,57 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+
         playerInput = GetComponent<PlayerInput>();
         playerTransform = GetComponent<Transform>();
         playerRB = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        playerSprite = GetComponent<SpriteRenderer>();
+        walkHashCode = Animator.StringToHash("Corriendo")
 
     }
 
     void Update()
     {
-
         horizontal = playerInput.ejeHorizontal;
         vertical = playerInput.ejeVertical;
-        backSide = (vertical > 0);
-        
+
         bool isVerticalZero = (vertical != 0);
 
-        if (isVerticalZero)
+        if (horizontal != 0 || vertical != 0)
         {
-            animator.SetBool("Back", backSide);
+            AssignXY();
+            animator.SetBool(walkHashCode, true);
+
+        }
+        else{
+            animator.SetBool(walkHashCode, false);
         }
 
+        SpriteFlipper();
+
+    }
+
+    private void SpriteFlipper()
+    {
+        if (horizontal < 0 && Mathf.Abs(vertical) < Mathf.Abs(horizontal))
+        {
+            playerSprite.flipX = true;
+        }
+        else if (horizontal != 0)
+        {
+            playerSprite.flipX = false;
+        }
+    }
+
+    private void AssignXY()
+    {
+        animator.SetFloat("X", horizontal);
+        animator.SetFloat("Y", vertical);
     }
 
     void FixedUpdate()
     {
-
-        // Vector2 newPosition = playerTransform.position + new Vector3(velocidad * horizontal*Time.deltaTime, velocidad * vertical*Time.deltaTime, 0);
-        // playerTransform.position = newPosition;
-
         Vector2 velocityVector = new Vector2(horizontal, vertical) * velocidad;
         playerRB.velocity = velocityVector;
 
